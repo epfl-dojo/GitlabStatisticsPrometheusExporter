4e7e5e39f8a38b7459b6f2e0af09a293611113bf
Nicolas Borboën <ponsfrilus@gmail.com>
2020-08-28 13:56:43 +0200

    [feature] Dockerfile
     - Now use a Dockerfile
     - Defines some ENV vars in docker-compose.yml
    - moar debug

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const fetch = require('node-fetch');
-
 const apiurl=process.env.GITLAB_URL+"/api/v4/application/statistics"
+const privateToken = process.env.PRIVATE_TOKEN
+
+// debug
+console.log(process.env.PRIVATE_TOKEN)
+console.log(process.env.GITLAB_URL)
+console.log(apiurl)
 
 app.get('/', (req, res) => {
-	fetch(apiurl)
-    	.then(res => res.json())
-    	.then(json => res.send(json))
-    	.catch( (e) => { res.send("Internal Error")})
+  fetch(apiurl, { headers: {'PRIVATE-TOKEN': privateToken}})
+    .then(res => res.json())
+    .then(json => res.send(json))
+    .catch( (e) => { 
+      console.error(e)
+      res.send("Internal Error")
+    })
 })
 
 app.listen(port, () => {
