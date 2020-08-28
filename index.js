c9e9d15a9ef4f571e5d41151ed4cd71eefa2c702
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2020-08-28 13:44:55 +0200

    wip

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const fetch = require('node-fetch');
+
+const apiurl=process.env.GITLAB_URL+"/api/v4/application/statistics"
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 1322')
+	fetch(apiurl)
+    	.then(res => res.json())
+    	.then(json => res.send(json))
+    	.catch( (e) => { res.send("Internal Error")})
 })
 
 app.listen(port, () => {
