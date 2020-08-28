ef481c631faab3716bdca0698a9188f7194d8da1
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2020-08-28 14:48:18 +0200

    [feature] json to metrics converter

--- index.js
+++ index.js
@@ -10,16 +10,27 @@ console.log(process.env.PRIVATE_TOKEN)
 console.log(process.env.GITLAB_URL)
 console.log(apiurl)
 
+function json_to_metrics(json) {
+  let metrics = ""
+  for (const [k, v] of Object.entries(json)) {
+    const l1 = `# HELP gitlab_${k}_total The total number or ${k}`
+    const l2 = `# TYPE gitlab_${k}_total counter`
+    const l3 = `gitlab_${k}_total ${v.replace(",", "")}`
+    metrics += [l1, l2, l3].join("\n") + "\n"
+  }
+  return metrics
+}
+
 app.get('/', (req, res) => {
   fetch(apiurl, { headers: {'PRIVATE-TOKEN': privateToken}})
-    .then(res => res.json())
-    .then(json => res.send(json))
-    .catch( (e) => { 
-      console.error(e)
-      res.send("Internal Error")
-    })
+  .then(res => res.json())
+  .then(json => res.send(json_to_metrics(json)))
+  .catch( (e) => { 
+    console.error(e)
+    res.send("Internal Error")
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Gitlab Metrics is listening on port ${port}`)
 })
